bebb530b91008c960ab5304c51a3b0c281d7c067
sgsk88 <sgsk88@nate.com>
2022-09-21 13:23:29 +0900

    거스름돈 다른방법

--- src/5585번 거스름돈.kt
+++ src/5585번 거스름돈.kt
@@ -6,49 +6,13 @@ fun main(){
     var b = 1000-a
     var sum = 0
 
-    while(true){
-        if(b%500 != 0){
-            sum += b/500
-            b %= 500
-        }else{
-            sum += b/500
-            break
-        }
-        if(b%100 != 0){
-            sum += b/100
-            b %= 100
-        }else{
-            sum += b/100
-            break
-        }
-        if(b%50 != 0){
-            sum += b/50
-            b %= 50
-        }else{
-            sum += b/50
-            break
-        }
-        if(b%10 != 0){
-            sum += b/10
-            b %= 10
-        }else{
-            sum += b/10
-            break
-        }
-        if(b%5 != 0){
-            sum += b/5
-            b %= 5
-        }else{
-            sum += b/5
-            break
-        }
-        if(b%1 != 0){
-            sum += b/1
-            b %= 1
-        }else{
-            sum += b/1
-            break
-        }
+    val list = mutableListOf<Int>(500, 100, 50, 10, 5, 1)
+
+    for(i in 0 until list.size){
+        sum += b/list[i]
+        if(b%list[i] == 0) break
+        b %= list[i]
+
     }
     print(sum)
 }
